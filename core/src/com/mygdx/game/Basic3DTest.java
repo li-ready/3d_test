0f3bc146c349345486d2e7d87f1a86eee77aca94
htli <aweosome9527@163.com>
2023-03-12 20:56:13 +0800

    3d游戏测试001

---
+++ core/src/com/mygdx/game/Basic3DTest.java
@@ -0,0 +1,32 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.ApplicationListener;
+public class Basic3DTest implements ApplicationListener {
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+	}
+
+	@Override
+	public void dispose () {
+	}
+
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void resize (int width, int height) {
+	}
+
+	@Override
+	public void pause () {
+	}
+}
