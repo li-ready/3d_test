02525d3cc62b9dbcf0b7f28fc05f2b67b2e32294
htli <aweosome9527@163.com>
2023-03-12 22:06:37 +0800

    渲染方块

--- core/src/com/mygdx/game/Basic3DTest.java
+++ core/src/com/mygdx/game/Basic3DTest.java
@@ -1,21 +1,60 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g3d.utils.*;
 public class Basic3DTest implements ApplicationListener {
+	public CameraInputController camController;
+	public PerspectiveCamera cam;
+	public Model model;
+	public ModelInstance instance;
+	public ModelBatch modelBatch;
+	public Environment environment;
 	@Override
 	public void create () {
+		modelBatch = new ModelBatch();
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
+		cam.update();
+		{//添加光照
+			environment = new Environment();
+			environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+			environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		}
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
 	}
 
 	@Override
 	public void render () {
+		camController.update();
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
 	}
 
 	@Override
 	public void dispose () {
+		modelBatch.dispose();
+		model.dispose();
 	}
 
 	@Override
