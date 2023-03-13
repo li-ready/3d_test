c7ac34cb67ddfe005d69728ebf64dcc1e575c682
htli <aweosome9527@163.com>
2023-03-13 16:36:36 +0800

    太空场景渲染

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,15 +2,13 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.example.plane3d;
-import com.mygdx.game.Basic3DTest;
-
+import com.example.LoadSceneTest;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("My GDX Game");
-		new Lwjgl3Application(new plane3d(), config);
+		new Lwjgl3Application(new LoadSceneTest(), config);
 	}
 }
