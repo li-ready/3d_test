b104f505fe64a910e91c6078f27ec470b14e8920
htli <aweosome9527@163.com>
2023-03-19 22:10:53 +0800

    象棋追鼠标002

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("My GDX Game");
+		//config.setBackBufferConfig(8, 8, 8, 8, 16, 0, 4);
 		new Lwjgl3Application(new xiangqitest(), config);
 	}
 }
